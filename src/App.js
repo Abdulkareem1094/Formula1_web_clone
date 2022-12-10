1e0fa8e4b938f1553bbc7d6391bc360b560a699c
abdul kareem <abdulak4t7@gmail.com>
2022-12-10 10:52:24 -0800

    done with chatengine

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ const App = () => {
   return (
    <ChatEngine 
       height ='100vh'
-      projectID= ''
-      userName = ''
+      projectID= 'e1c79b95-14c1-46f5-8e61-86602479fe35'
+      userName = 'Abdulkareem'
+      userSecret= 'secret'
    />
   );
 }
